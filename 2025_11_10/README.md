bdbe2158e1d66f2e517f91d6c798b2f75d212de6
jchiroques <jchiroques@uni.pe>
2025-11-10 01:39:40 -0500

    feat: ultima prueba de hoy.

--- 2025_11_10/README.md
+++ 2025_11_10/README.md
@@ -1,2 +1,4 @@
 # git y gihub
 ## subtitle
+
+Se creo la funcion de finbonacci
--- 2025_11_10/main.py
+++ 2025_11_10/main.py
@@ -13,4 +13,13 @@ print("Hola" , variable_3)
 otra_variable = "Hola"
 print(otra_variable)
 
+# crear funcion de fibonacci
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
 # Este es un numero comentario
