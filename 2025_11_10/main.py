4c46d8fd4f16e5ad74d1597044c0a9c1ee8e2ba8
jchiroques <jchiroques@uni.pe>
2025-11-10 01:32:57 -0500

    feat: add new comment

--- 2025_11_10/main.py
+++ 2025_11_10/main.py
@@ -2,4 +2,15 @@
 variable = "Jose"
 print("Hola" , variable)
 
+# Vamos a hacer un camvio más
+variable_2 = "Marcos"
+print("Hola" , variable_2)
+
+# Vamos a hacer un camvio más
+variable_3 = "Juan"
+print("Hola" , variable_3)
+
+otra_variable = "Hola"
+print(otra_variable)
+
 # Este es un numero comentario
